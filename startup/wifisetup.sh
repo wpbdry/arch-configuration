23071a934139a6c3bb128e1ab2d14c82059b1ecb
William Dry <wpbdry@gmail.com>
2019-05-13 00:43:14 +0200

    improve error handling in wifisetup.sh

--- startup/wifisetup.sh
+++ startup/wifisetup.sh
@@ -10,6 +10,7 @@
 PARENTDIRECTORY='/etc/netctl/'
 FILENAMESUFFIX='-wifi'
 
+
 # In case user types invalid wifi name
 NAMEISVALID=1
 while [ $NAMEISVALID -ne 0 ]
@@ -39,5 +40,27 @@ do
 	fi
 done
 
+
 # Connect to wifi
-netctl start $FILENAME
+
+# Check if wifi is already active and do nothing
+WIFISTATE=$(netctl is-active $FILENAME)
+if [ $WIFISTATE == 'active' ]
+then
+	echo 'You are already connected to' $WIFINAME
+	echo 'Doing nothing.'
+	exit 0
+fi
+
+# In case other wifi network is active. To avoid errors
+echo 'Disconnecting from other networks...'
+sudo netctl stop-all
+
+# Connect to wifi
+echo 'Connecting to' $WIFINAME
+sudo netctl start $FILENAME
+
+if [ $? -eq 0 ]
+then
+	echo 'Successfully connected to' $WIFINAME
+fi
