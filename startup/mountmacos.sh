02d439a3939610e104b5e423f27db328a34ce303
William Dry <wpbdry@gmail.com>
2019-05-14 23:45:22 +0200

    improve variable names in sh files

--- startup/mountmacos.sh
+++ startup/mountmacos.sh
@@ -5,16 +5,16 @@
 #!/bin/bash
 
 # Settings
-PARTUUID='39ff1bc9-2aa6-42aa-8596-a3323a50f1be'
-MOUNTPOINT='/MacOS'
+partition_id='39ff1bc9-2aa6-42aa-8596-a3323a50f1be'
+mountpoint='/MacOS'
 
 # Mount partition
 echo 'Mounting MacOS partition...'
-PARTNAME=$(findfs UUID=$PARTUUID)
-sudo apfs-fuse -o allow_other $PARTNAME $MOUNTPOINT
+partition_name=$(findfs UUID=$partition_id)
+sudo apfs-fuse -o allow_other $partition_name $mountpoint
 
 # Check success
 if [ $? -eq 0 ]
 then
-	echo $PARTNAME 'successfully mounted as read-only at' $MOUNTPOINT
+	echo $partition_name 'successfully mounted as read-only at' $mountpoint
 fi
--- startup/wifisetup.sh
+++ startup/wifisetup.sh
@@ -7,36 +7,36 @@
 # All wifi configuration files are named like *-wifi
 # Wifi configuration files are in /etc/netctl
 
-PARENTDIRECTORY='/etc/netctl/'
-FILENAMESUFFIX='-wifi'
+parent_directory='/etc/netctl/'
+filename_suffix='-wifi'
 
 
 # In case user types invalid wifi name
-NAMEISVALID=1
-while [ $NAMEISVALID -ne 0 ]
+name_is_valid=1
+while [ $name_is_valid -ne 0 ]
 do
 	# Welcome message
 	echo "Please type wifi name. Type 'skip' to skip this step."
 
 	# Get input
-	read WIFINAME
-	WIFINAME=$(echo "$WIFINAME" | tr '[:upper:]' '[:lower:]')
+	read wifi_name
+	wifi_name=$(echo "$wifi_name" | tr '[:upper:]' '[:lower:]')
 
 	# Check for 'skip'
-	if [ $WIFINAME == 'skip' ]
+	if [ $wifi_name == 'skip' ]
 	then
 		echo 'Skipped wifi setup.'
 		exit 0
 	fi
 
 	# Check if file exists for this wifi
-	FILENAME=$WIFINAME$FILENAMESUFFIX
-	test -f $PARENTDIRECTORY$FILENAME
-	NAMEISVALID=$?
+	filename=$wifi_name$filename_suffix
+	test -f $parent_directory$filename
+	name_is_valid=$?
 
-	if [ $NAMEISVALID -ne 0 ]
+	if [ $name_is_valid -ne 0 ]
 	then
-		echo 'No configuration file found for wifi network' $WIFINAME
+		echo 'No configuration file found for wifi network' $wifi_name
 	fi
 done
 
@@ -44,10 +44,10 @@ done
 # Connect to wifi
 
 # Check if wifi is already active and do nothing
-WIFISTATE=$(netctl is-active $FILENAME)
-if [ $WIFISTATE == 'active' ]
+wifi_state=$(netctl is-active $filename)
+if [ $wifi_state == 'active' ]
 then
-	echo 'You are already connected to' $WIFINAME
+	echo 'You are already connected to' $wifi_name
 	echo 'Doing nothing.'
 	exit 0
 fi
@@ -57,10 +57,10 @@ echo 'Disconnecting from other networks...'
 sudo netctl stop-all
 
 # Connect to wifi
-echo 'Connecting to' $WIFINAME
-sudo netctl start $FILENAME
+echo 'Connecting to' $wifi_name
+sudo netctl start $filename
 
 if [ $? -eq 0 ]
 then
-	echo 'Successfully connected to' $WIFINAME
+	echo 'Successfully connected to' $wifi_name
 fi
